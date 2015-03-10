e99a95c6de3cab9b0deaa64d0396ea092967224d
mlpinlac <mlpinlac@hawaii.edu>
2015-03-09 15:55:34 -1000

    Played with power on input
    it does not work…. just fyi

---
+++ 491Project/Assets/FireHitAction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireHitAction : MonoBehaviour {
+
+	public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animation> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("a"))
+		{
+			anim.Play("fire_hit");
+	
+		}
+		//if (!Input.GetKey ("a")) {
+		//	anim.Play ()
+		/// play idle animation
+		//}
+}
+}
