5ea64cd719fa909c9a64830e69b630974b1020df
mlpinlac <mlpinlac@hawaii.edu>
2015-03-17 14:31:52 -1000

    Added Spawner
    - random spawner
    - spawns fishies
    - *need to fix fishes moving left to right, does not respawn*

---
+++ 491Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+
+	private GameObject[] gameObjPool;
+	private Queue<GameObject> availableGOQueue = new Queue<GameObject>();
+	private Camera cam;
+	
+	// These are used to determin spawning frequency
+	public float minSpawnTime = 1.0f;
+	public float maxSpawnTime = 10.0f;
+	
+	// Spawning offset (relative to the Spawner object)
+	public float spawningOffsetTop = 0f;
+	public float spawningOffsetBottom = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+		gameObjPool = new GameObject[transform.childCount];
+		cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+		int i = 0;
+		foreach(Transform childTr in this.transform){
+			// Populate gameObjPool by fetching the children list of the spawner
+			gameObjPool[i] = childTr.gameObject;
+			// Set object position = to parent's and make it invisible
+			resetObject(gameObjPool[i]);
+			i++;
+		}
+		// Then call the spawning method
+		Invoke("spawnObject", Random.Range(minSpawnTime, maxSpawnTime));
+
+		// to spawn objects at fixed rate, have to set the same values for minSpawnTime and maxSpawnTime: 
+		//for instance, if we want to spawn objects every 1 second we set minSpawnTime = maxSpawnTime = 1
+	}
+
+	// This actualy does the job...
+	void spawnObject(){
+		// First check for objects out of view
+		for(int i=0; i < gameObjPool.Length; i++){
+			// Convert object position to screen point, so that we can test object position relatively to camera viewport
+			Vector3 screenPt = cam.WorldToScreenPoint(gameObjPool[i].transform.position);
+			// Left border
+			if(gameObjPool[i].activeSelf == true && screenPt.x < 0){
+				resetObject(gameObjPool[i]);
+			}
+
+			// Right border
+			if(gameObjPool[i].activeSelf == true && screenPt.x == 0){
+				resetObject(gameObjPool[i]);
+			}
+		}
+		
+		// Spawn an object only if available
+		if(availableGOQueue.Count > 0){
+			GameObject gameObj = availableGOQueue.Dequeue();
+			// Set random offset...
+			gameObj.transform.Translate(Vector2.up * Random.Range(spawningOffsetTop, spawningOffsetBottom));
+			// Activate the object
+			gameObj.SetActive(true);
+		}
+		// Recursively call this method randomly
+		Invoke("spawnObject", Random.Range(minSpawnTime, maxSpawnTime));
+	}
+
+	public void resetObject(GameObject obj){
+		// Deactivate object, reset its position and enqueue it for another spawn
+		obj.SetActive(false);
+		obj.transform.position = this.transform.position;
+		availableGOQueue.Enqueue(obj);
+	}
+}
