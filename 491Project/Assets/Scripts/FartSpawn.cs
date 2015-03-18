5597c69dd15cde70880879add8c8f830bcfa8787
mlpinlac <mlpinlac@hawaii.edu>
2015-03-18 12:52:47 -1000

    Spawn on space
    - fart bubbles spawn whenever you hit space bar

---
+++ 491Project/Assets/Scripts/FartSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FartSpawn : MonoBehaviour {
+
+	public Vector3 ObjectSpawnPosition;
+	public GameObject obj;
+	//public GameObject Player;
+	
+	void Update(){
+		ObjectSpawnPosition = GameObject.FindWithTag("Player").transform.position;
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			Instantiate(obj, ObjectSpawnPosition, Quaternion.identity);
+		}
+	}
+}
+
