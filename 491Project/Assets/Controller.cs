83f07315d268254d94051930350870ba9c644f88
DoDavid <dodavid@hawaii.edu>
2015-03-26 03:58:40 -1000

    Whale Surfacing and filling air meter
    Currently working when the whale surfaces, it fills the air meter.
    it’s 4am and I want to sleep.  Gnite.

--- 491Project/Assets/Controller.cs
+++ 491Project/Assets/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour {
 	
@@ -7,6 +8,36 @@ public class Controller : MonoBehaviour {
 	public bool facingRight = false;
 	public bool facingUp = false;
 	public RectTransform healthTransform;
+	private float cachedY; // stores Y position
+	private float minXValue;
+	private float maxXvalue;
+	public int currentHealth;
+	
+	private int CurrentHealth
+	{
+		get { return currentHealth;}
+		set { 
+			currentHealth = value; 
+			HandleHealth();
+		}
+	}
+	
+	public int maxHealth;
+	public Text healthText;
+	public Image visualHealth;
+	public float coolDown;
+	private bool onCD;
+	public Canvas canvas;
+	
+	void Start()
+	{
+		onCD = false;
+		cachedY = healthTransform.position.y;
+		maxXvalue = healthTransform.position.x;
+		minXValue = healthTransform.position.x - healthTransform.rect.width * canvas.scaleFactor;
+		currentHealth = maxHealth;
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -27,12 +58,61 @@ public class Controller : MonoBehaviour {
 			transform.Translate(0, -maxSpeed * Time.deltaTime,0);
 		}
 		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			
+			if(!onCD && currentHealth > 0)
+			{
+				StartCoroutine(CoolDownDmg());
+				CurrentHealth -= 10;
+			}
+			
+		}
+		
 		if (h > 0 && !facingRight) 
 			Flip ();
-
+		
 		else if(h < 0 && facingRight)
 			Flip();
 		
+	}
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "AirBar") {
+			currentHealth = maxHealth;
+		}
+		if (col.gameObject.tag == "urchin") {
+			if(!onCD && currentHealth > 0)
+			{
+				StartCoroutine(CoolDownDmg());
+				CurrentHealth -= 10;
+			}
+		}
+	}
+	private void HandleHealth()
+	{
+		healthText.text = "Air: " + currentHealth;
+		float currentXValue = MapValues (currentHealth, 0, maxHealth, minXValue, maxXvalue);
+		healthTransform.position = new Vector3 (currentXValue, cachedY);
+		
+		if (currentHealth > maxHealth / 2) {  // Then i have more than 50% health
+			visualHealth.color = new Color32((byte) MapValues(currentHealth, maxHealth/2, maxHealth,255,0),255,0,255);
+		} 
+		else // less than 50%
+		{
+			visualHealth.color = new Color32(255, (byte)MapValues(currentHealth, 0, maxHealth/2, 0, 255), 0, 255);
+		}
+	}
+	
+	IEnumerator CoolDownDmg()
+	{
+		onCD = true;
+		yield return new WaitForSeconds(coolDown);
+		onCD = false;
+	}
+	private float MapValues(float x, float inMin, float inMax, float outMin, float outMax)
+	{
+		return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 	}
 	
 	void Flip ()
@@ -45,5 +125,5 @@ public class Controller : MonoBehaviour {
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
-
+	
 }
---
+++ 491Project/Assets/FillAir.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FillAir : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collision2D col)
+	{
+		Controller cont = GetComponent<Collider>().gameObject.GetComponent<Controller>();
+		if (cont.gameObject.tag == "Player") {
+			cont.currentHealth = cont.maxHealth;
+			
+		}
+	}
+}
