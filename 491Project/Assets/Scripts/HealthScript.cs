2973b59836cee888654de0b5e9cb725ade233634
DoDavid <dodavid@hawaii.edu>
2015-03-26 00:10:43 -1000

    Fart collision with fish.
    Added the fart collision with fish.

---
+++ 491Project/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	public int hp = 1;
+
+	public bool isEnemy = true;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
+
+		if (shot != null) {
+			if (shot.isEnemyShot != isEnemy) {
+				hp -= shot.damage;
+				Destroy (shot.gameObject);
+
+				if (hp <= 0) {
+					Destroy (gameObject);
+				}
+			}
+		}
+	}
+}
