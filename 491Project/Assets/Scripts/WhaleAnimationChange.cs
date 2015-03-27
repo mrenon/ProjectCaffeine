d557cb35f4db047bec1f01c12b599525904d6c37
mrenon <mrenon@hawaii.edu>
2015-03-27 09:42:05 -1000

    whale animations
    -changed the whale to the new whale animation
    - changed the whale animation on up key
    -changed the whale animation on down key
    -changed the whale animation to fart on space key

---
+++ 491Project/Assets/Scripts/WhaleAnimationChange.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhaleAnimationChange : MonoBehaviour {
+
+	
+	Animator anim;
+	bool upSwim;
+	bool downSwim;
+	bool fartFace;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("down")) {
+			anim = GetComponent<Animator> ();
+			anim.SetBool ("downSwim", true);
+			downSwim = anim.GetBool ("downSwim");
+			if(Input.GetKey("space")){
+				anim = GetComponent<Animator>();
+				anim.SetBool("fartFace", true);
+				fartFace = anim.GetBool("fartFace");
+			}
+			//anim.SetTrigger("downSwimTrig");
+		} else if (Input.GetKey ("up")) {
+			anim = GetComponent<Animator> ();
+			anim.SetBool ("upSwim", true);
+			upSwim = anim.GetBool ("upSwim");
+			if(Input.GetKey("space")){
+				anim = GetComponent<Animator>();
+				anim.SetBool("fartFace", true);
+				fartFace = anim.GetBool("fartFace");
+			}
+			//anim.SetTrigger("upSwimTrig");
+		} else if (Input.GetKey ("space")) {
+			anim = GetComponent<Animator>();
+			anim.SetBool("fartFace", true);
+			fartFace = anim.GetBool("fartFace");
+		}
+		else if(!Input.GetKey ("down") || !Input.GetKey("up") ||
+		        !Input.GetKey("space")) {
+			anim.SetBool("downSwim", false);
+			anim.SetBool("upSwim", false);
+			anim.SetBool("fartFace",false);
+			downSwim = anim.GetBool("downSwim");
+			upSwim = anim.GetBool("upSwim");
+			fartFace = anim.GetBool("fartFace");
+		}
+		
+	}
+}
