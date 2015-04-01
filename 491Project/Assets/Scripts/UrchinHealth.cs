0319b4dc1cec33c484c5bde29ddde638664205f8
DoDavid <dodavid@hawaii.edu>
2015-04-01 03:16:06 -1000

    Fix urchin code

--- 491Project/Assets/Scripts/UrchinHealth.cs
+++ 491Project/Assets/Scripts/UrchinHealth.cs
@@ -5,14 +5,14 @@ public class UrchinHealth : MonoBehaviour {
 
 	public int hp = 1000;
 	public bool isEnemy = true;
-<<<<<<< HEAD
+
 
 	void Update() {
 		GetComponent<Collider>().enabled = true;
 	}
-=======
+
 	public AudioSource fartPop;
->>>>>>> origin/David
+
 	
 	void OnTriggerEnter2D(Collider2D collider) {
 		ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
