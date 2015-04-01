7423b73ace425dd3fc5efa27ec09a807be723d0b
DoDavid <dodavid@hawaii.edu>
2015-03-31 23:12:23 -1000

    Fix Bubbles from getting stuck at surface
    Bubbles were getting all stuck at the top collider.  Fixed it.

---
+++ 491Project/Assets/DestroyAir.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAir : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "air") {
+			Destroy (col.gameObject);
+			//	fishTotal = fishTotal + 1;
+		}
+	}
+}
