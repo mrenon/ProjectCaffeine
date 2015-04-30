42722ea64f7e79eb9d4353f4aae4f7ad09d05248
DoDavid <dodavid@hawaii.edu>
2015-04-29 20:30:46 -1000

    Added Parallaxing effect
    Now the sky will transition from day to night and back to day again
    continuously.

---
+++ 491Project/Assets/Scripts/Parallax.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public float speed = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
+	}
+}
