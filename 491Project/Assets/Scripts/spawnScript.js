da7112d5b95754d59225954c47d92c05a5ef2fc5
DoDavid <dodavid@hawaii.edu>
2015-05-04 11:53:22 -1000

    tweak randomness
    fishes, enemy and bubble are a lot more randomly generated.  bubbles
    are not easily to obtain making the user more concern with conserving
    bubbles.

--- 491Project/Assets/Scripts/spawnScript.js
+++ 491Project/Assets/Scripts/spawnScript.js
@@ -8,18 +8,18 @@ var tempnight : GameObject;
 
 // Variable to know how fast we should create new enemies
 public var spawnTime : float = 0;
-var number = Random.Range(9,9);
+var number = Random.Range(1,9);
 
 function Start() {  
     // Call the 'addEnemy' function every 'spawnTime' seconds
-    number = Random.Range(5,9);
+    number = Random.Range(9,9);
     InvokeRepeating("addEnemy", number, number);
     tempenemy = enemy;
 	tempnight = nightenemy;
 }
 
 function Update() {
-
+	number = Random.Range(1,9);
 	timer -= Time.deltaTime;
 	if(timer <= 0) {
 		enemy = tempnight;
@@ -36,7 +36,7 @@ function Update() {
 }
 
 function FixedUpdate() {
-	number = Random.Range(5,9);
+	//number = Random.Range(5,9);
 }
 
 // New function to spawn an enemy
@@ -50,5 +50,7 @@ function addEnemy() {
     var spawnPoint = new Vector2(Random.Range(x1, x2), transform.position.y);
 
     // Create an enemy at the 'spawnPoint' position
+    if(spawnTime == number || spawnTime == 9 || spawnTime == 7 || spawnTime == 2 || spawnTime == 4 || spawnTime == 1 || spawnTime == 6) {
     Instantiate(enemy, spawnPoint, Quaternion.identity);
+    }
 }
