962152487ff11e8ca03b59307fb1284dc2cb042e
mlpinlac <mlpinlac@hawaii.edu>
2015-05-03 16:50:32 -1000

    Fixed Parallax Pause

--- 491Project/Assets/Scripts/ParallaxWater.cs
+++ 491Project/Assets/Scripts/ParallaxWater.cs
@@ -4,26 +4,35 @@ using System.Collections;
 public class ParallaxWater : MonoBehaviour {
 
 	public float speed = 0;
-	Controller contPause = new Controller();
+	public float hiddenTime;
 
 	// Use this for initialization
 	void Start () {
+		//contPause.timeRemaining = 99999;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		hiddenTime -= Time.deltaTime;
+
+		if (hiddenTime > 0) {
+			GetComponent<Renderer>().material.mainTextureOffset += new Vector2 (Time.deltaTime * speed, 0f);
+		}
+		else if (hiddenTime <= 0) {
+			PauseScroll ();
+		} 
+		//PauseScroll ();
 		//GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
-		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
-		PauseScroll ();
+
 	}
 	void PauseScroll()
 	{
-		if (contPause.timeRemaining == 99990) {
-			StartCoroutine (PauseTime (1)); 
-		}
+
+			StartCoroutine (PauseTime (1.0f)); 
+
 	}
 	
-	IEnumerator PauseTime(float blinkTime)
+	/*IEnumerator PauseTime(float blinkTime)
 	{
 		GetComponent<Renderer>().material.mainTextureOffset += new Vector2 (Time.deltaTime * speed, 0f);
 		yield return new WaitForSeconds(blinkTime);
@@ -32,5 +41,14 @@ public class ParallaxWater : MonoBehaviour {
 		yield return new WaitForSeconds(12.0f);
 		speed = 0.03f;
 		GetComponent<Renderer>().material.mainTextureOffset += new Vector2 (Time.deltaTime * speed, 0f);
+	}*/
+	IEnumerator PauseTime(float blinkTime)
+	{
+		speed = 0f;
+		GetComponent<Renderer>().material.mainTextureOffset += new Vector2 (Time.deltaTime * speed, 0f);
+		yield return new WaitForSeconds(30.0f);
+		speed = 0.015f;
+		GetComponent<Renderer>().material.mainTextureOffset += new Vector2 (Time.deltaTime * speed, 0f);
+		hiddenTime = 20.0f;
 	}
 }
