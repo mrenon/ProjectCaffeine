0be78415bb1903ca518a51b9907c320624d60037
DoDavid <dodavid@hawaii.edu>
2015-05-03 23:25:56 -1000

    spawn boat is working

--- 491Project/Assets/Scripts/spawnScriptBoat.js
+++ 491Project/Assets/Scripts/spawnScriptBoat.js
@@ -3,32 +3,43 @@ public var enemy : GameObject;
 public var nightenemy : GameObject;
 public var timer : float;
 var tempenemy : GameObject;
-var tempnight : GameObject;
+var tempnight : GameObject;	
+var check = 1;
 
 
 // Variable to know how fast we should create new enemies
 public var spawnTime : float = 0;
 var number = Random.Range(9,9);
 
+
 function Start() {  
     // Call the 'addEnemy' function every 'spawnTime' seconds
-    number = Random.Range(5,9);
-    InvokeRepeating("addEnemy", number, number);
+    number = Random.Range(1,0);
+  //  InvokeRepeating("addEnemy", number, number);
     tempenemy = enemy;
 	tempnight = nightenemy;
+
 }
 
 function Update() {
 
 	timer -= Time.deltaTime;
 	if(timer <= 0) {
+	
 		enemy = tempnight;
+		if(check == 1) {
+		Instantiate(enemy, transform.position, transform.rotation);
+		check = 0;
+		}
 	}
 	
 	if(timer <= -35) {
 		while(timer <= 0) {
+		
+			
 			timer += 67;
 			enemy = tempenemy;
+			check = 1;
 		}
 	}
 }
@@ -48,5 +59,5 @@ function addEnemy() {
     var spawnPoint = new Vector2(Random.Range(x1, x2), transform.position.y);
 
     // Create an enemy at the 'spawnPoint' position
-    Instantiate(enemy, spawnPoint, Quaternion.identity);
+  //  Instantiate(enemy, spawnPoint, Quaternion.identity);
 }
